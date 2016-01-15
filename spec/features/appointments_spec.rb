ffd3fa410f1a3b49e32380dc364c8234c2edfaa7
ipc103 <iancandysemail@gmail.com>
2016-01-15 13:17:22 -0500

    add appointment specs

--- spec/features/appointments_spec.rb
+++ spec/features/appointments_spec.rb
@@ -1,34 +1,24 @@
 describe "appointments", type:  :feature do
   before do
-    Doctor.create([
-      {name: "Meredith Grey", department: "Internal Medicine"},
-      {name: "Hawkeye Pierce", department: "Surgery"},
-      {name: "Leonard 'Bones' McCoy", department: "Internal Medicine"},
-      {name: "Phillip Chandler", department: "Internal Medicine"},
-      {name: "Michaela Quinn", department: "Internal Medicine"}
-    ])
-
-    Patient.create([
-      {name: "Homer Simpson", age:38},
-      {name: "Bart Simpson", age:10},
-      {name: "Marge Simpson", age:36},
-      {name: "Lisa Simpson", age:8},
-      {name: "Maggie Simpson", age:1},
-    ])
-
-    Appointment.create([
-      {appointment_datetime: DateTime.new(2016, 03, 15, 18, 00, 0), patient_id: 1, doctor_id: 2},
-      {appointment_datetime: DateTime.new(2016, 01, 11, 20, 20, 0), patient_id: 2, doctor_id: 1},
-      {appointment_datetime: DateTime.new(2016, 04, 22, 14, 00, 0), patient_id: 3, doctor_id:5 },
-      {appointment_datetime: DateTime.new(2017, 10, 30, 15, 00, 0), patient_id: 4, doctor_id:4 },
-      {appointment_datetime: DateTime.new(2016, 07, 11, 16, 00, 0), patient_id: 5, doctor_id: 3},
-      {appointment_datetime: DateTime.new(2016, 05, 31, 17, 00, 0), patient_id: 1, doctor_id: 2},
-      {appointment_datetime: DateTime.new(2017, 06, 03, 10, 00, 0), patient_id: 3, doctor_id: 1}
-      ])
-
-      @meredith = Doctor.first
-      @bart = Patient.find(2)
+    @hawkeye = Doctor.create({name: "Hawkeye Pierce", department: "Surgery"})
+    @homer = Patient.create({name: "Homer Simpson", age:38})
+    @appointment = Appointment.create({appointment_datetime: DateTime.new(2016, 03, 15, 18, 00, 0), patient: @homer, doctor: @hawkeye})
   end
+
+  it "should display an appontment's doctor" do
+    visit appointment_path(@appointment)
+    expect(page).to have_link("Hawkeye Pierce", href: doctor_path(@hawkeye))
+  end
+
+  it "should display an appontment's patient" do
+    visit appointment_path(@appointment)
+    expect(page).to have_link("Homer Simpson", href: patient_path(@homer))
+  end
+
+  it "should not have an index page" do
+    expect {visit('/appointments')}.to raise_error(ActionController::RoutingError)
+  end
+
 
 
 end
