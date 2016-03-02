0e21c8c8f743f8bc4af462075482c0fc2a3a174b
jaeku <ja.ku@rutgers.edu>
2016-03-02 21:53:22 +0000

    Done.

---
+++ app/models/appointment.rb
@@ -0,0 +1,16 @@
+class Appointment < ActiveRecord::Base
+  belongs_to :doctor
+  belongs_to :patient
+
+  def readable_datetime
+    self.appointment_datetime.strftime('%B %d, %Y at %H:%M')
+  end
+
+  def doctor_name
+    self.doctor.name
+  end
+
+  def patient_name
+    self.patient.name
+  end
+end
