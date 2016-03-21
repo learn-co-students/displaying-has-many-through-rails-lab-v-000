e89ae50370490808f6ffcf35e8f18642c40bcb31
Chris Bruen <bruen.chris@gmail.com>
2016-03-20 20:23:14 -0400

    done

---
+++ app/controllers/appointments_controller.rb
@@ -0,0 +1,21 @@
+class AppointmentsController < ApplicationController
+	
+
+	
+	def show
+		@appointment = Appointment.find(params[:id])
+	end
+	
+	def new
+	end
+	
+	def create
+	end
+	
+	def edit
+	end
+	
+	def update
+	end
+	
+end
