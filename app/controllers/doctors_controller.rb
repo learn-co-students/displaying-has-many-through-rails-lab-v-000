2196e4a6cdd41e087e40f194da2de3654a52e542
Dakota Martinez <dakotaleemusic@gmail.com>
2016-03-23 22:24:08 +0000

    adds index and show actions and pages for doctors

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,2 +1,17 @@
 class DoctorsController < ApplicationController
+  before_action :set_doctor, only: [:show, :edit, :update, :destroy]
+  
+  def index 
+    @doctors = Doctor.all
+  end
+  
+  def show 
+    
+  end
+  
+  private 
+  
+  def set_doctor
+    @doctor = Doctor.find(params[:id])
+  end
 end
