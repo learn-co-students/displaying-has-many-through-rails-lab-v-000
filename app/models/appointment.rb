0a28e2c32f851dcd67f137568b3ace6311dec275
shinyrachel <rachel.kory@gmail.com>
2016-03-30 15:12:44 -0400

    patients and doctors features specs passing

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,16 @@
 class Appointment < ActiveRecord::Base
   belongs_to :patient
   belongs_to :doctor
+
+  def readable_time
+    appointment_datetime.to_s(:appt_time)
+  end
+
+  def patient_name
+    patient.name
+  end
+
+  def doctor_name
+    doctor.name
+  end
 end
