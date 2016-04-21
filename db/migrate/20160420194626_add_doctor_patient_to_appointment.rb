8f4e2bb158684d24d6491e3f2b872de5d7d1961b
tejbans <singh.tejbans@gmail.com>
2016-04-21 12:00:25 -0700

    Done.

---
+++ db/migrate/20160420194626_add_doctor_patient_to_appointment.rb
@@ -0,0 +1,6 @@
+class AddDoctorPatientToAppointment < ActiveRecord::Migration
+  def change
+    add_column :appointments, :doctor_id, :integer
+    add_column :appointments, :patient_id, :integer
+  end
+end
