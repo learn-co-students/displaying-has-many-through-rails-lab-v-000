0deca539f89af2beed97fdfa493b0b1551f4d292
evanscloud <dijkstrascloud@gmail.com>
2016-04-21 11:02:33 -0400

    display hospital data using associations

---
+++ app/controllers/patients_controller.rb
@@ -0,0 +1,28 @@
+class PatientsController < ApplicationController
+  def index
+    @patients = Patient.all
+  end
+
+  def new
+    @patient = Patient.new
+  end
+
+  def create
+    @patient = Patient.create(patient_params)
+    if @patient.save
+      redirect_to patient_path(@patient)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @patient = Patient.find(params[:id])
+  end
+
+  private
+
+  def patient_params
+    params.require(:patient).permit(:name, :age)
+  end
+end
