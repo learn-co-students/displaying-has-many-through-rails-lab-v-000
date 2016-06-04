b40ddcd6178cbefbbb9104e0c000de4c1608f33e
lawrend <douglaslawrence314@gmail.com>
2016-06-03 21:25:27 -0700

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 
-  resources :appointments
-  resources :doctors
-  resources :patients
-  resources :patients
-  resources :doctors
   resources :appointments, only: [:show]
+  resources :doctors
+  resources :patients
+  resources :patients
+  resources :doctors
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- spec/features/doctors_spec.rb
+++ spec/features/doctors_spec.rb
@@ -1,6 +1,8 @@
+require 'rails_helper'
+
 describe "doctors", type:  :feature do
   before do
-    @meredith = Doctor.create({name: "Meredith Grey", department: "Internal Medicine"})
+    @meredith = Doctor.create(name: "Meredith Grey", department: "Internal Medicine")
     @bart = Patient.create(name: "Bart Simpson", age:10 )
     Appointment.create(appointment_datetime: DateTime.new(2016, 01, 11, 20, 20, 0), patient_id: 1, doctor_id: 1)
   end
