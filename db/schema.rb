bf85df3c020eb9501b3aefe6b37b8186506c81ac
lawrend <douglaslawrence314@gmail.com>
2016-06-03 19:45:05 -0700

    doctor index working

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160604022108) do
+ActiveRecord::Schema.define(version: 20160604023815) do
 
   create_table "appointments", force: :cascade do |t|
     t.datetime "appointment_datetime"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20160604022108) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "age"
   end
 
 end
