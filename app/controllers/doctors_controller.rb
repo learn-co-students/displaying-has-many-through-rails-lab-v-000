77feb531d81d6a9e23b3e5da1b6cfc6922a5f83b
PeteAJ <peter.jacobson@gmail.com>
2016-06-09 15:36:35 -0400

    Done.

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,17 +1,17 @@
 class DoctorsController < ApplicationController
-  def index
+
+def index
     @doctors = Doctor.all
   end
+
+
+
 
   def show
     @doctor = Doctor.find(params[:id])
   end
 
-  private
-
-  def doctor_params
-    params.require(:doctor).permit(:name)
-  end
+ 
 
 
 end
