99f02f8647f044e14fe9f48f04be922e16d2d1ef
jwolfe890 <jwolfe890@gmail.com>
2016-08-24 02:21:44 -0400

    Done.

---
+++ app/controllers/patients_controller.rb
@@ -0,0 +1,18 @@
+class PatientsController < ApplicationController
+
+def index
+  @patients = Patient.all
+end
+
+def new
+  @patient = Patient.new
+end 
+
+def create
+end 
+
+def show
+  @patient = Patient.find(params[:id])
+end 
+
+end
---
+++ app/models/patient.rb
@@ -0,0 +1,6 @@
+class Patient < ActiveRecord::Base
+
+has_many :doctors, through: :appointments
+has_many :appointments
+
+end
