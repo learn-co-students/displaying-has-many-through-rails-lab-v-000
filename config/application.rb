0e9a54a810e0a380601ec242bfd6dd8980e9919d
Dom McKellar <dom.mckellar@gmail.com>
2016-08-29 12:34:14 -0700

    Done.

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,13 @@ Bundler.require(*Rails.groups)
 
 module BasicNestedFormsLab
   class Application < Rails::Application
+    #place in config/application.rb file
+    config.generators do |g|
+      g.test_framework  nil #to skip test framework
+      g.assets  false
+      g.helper false
+      g.stylesheets false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
