e5e2baf7edb55cf4927b876b7fd28caf40857d43
Kathleen <kayjsmyth@gmail.com>
2016-10-07 16:14:06 -0400

    Done.

--- spec/features/doctors_spec.rb
+++ spec/features/doctors_spec.rb
@@ -3,6 +3,7 @@ require 'pry'
 describe "doctors", type: :feature do
   before do
     @meredith = Doctor.create({name: "Meredith Grey", department: "Internal Medicine"})
+    #binding.pry
     @bart = Patient.create(name: "Bart Simpson", age:10 )
     Appointment.create(appointment_datetime: DateTime.new(2016, 01, 11, 20, 20, 0), patient_id: 1, doctor_id: 1)
   end
