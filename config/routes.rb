95f5b3039abbdf230c6f7b66b0d482dbb315de1c
William Jones <enquiries@forestfed.com>
2016-10-23 01:05:47 +0000

    has many through lab

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
 
-  resources :appointments
-  resources :patients
-  resources :doctors
   resources :doctors
   resources :patients
   resources :appointments, only: [:show]
+  # hello
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
