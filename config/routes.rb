998e301355ab169174e4dca4c80e886b9a74aadb
Jennifer Pazos <jmp521@gmail.com>
2016-12-26 20:18:50 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
 
-  resources :appointments
-  resources :appointments
-  resources :patients
+
+  
   resources :doctors
-  resources :doctors
+
   resources :patients
   resources :appointments, only: [:show]
   # The priority is based upon order of creation: first created -> highest priority.
