4e52129c4836af3f0695672ff5f788a7617ef929
Ray Mulligan <rmullig2@optonline.net>
2017-01-09 16:48:14 -0500

    Done.

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,5 +1,4 @@
 class Patient < ActiveRecord::Base
-  has_many :appointments,
-    :foreign_key => "id"
+  has_many :appointments
   has_many :doctors, through: :appointments
 end
--- db/migrate/20170108192052_create_appointments.rb
+++ db/migrate/20170108192052_create_appointments.rb
@@ -2,10 +2,8 @@ class CreateAppointments < ActiveRecord::Migration
   def change
     create_table :appointments do |t|
       t.datetime :appointment_datetime
-      t.belongs_to :doctor,
-        :class_name => 'Doctor', :foreign_key => 'id'
-      t.belongs_to :patient,
-        :class_name => 'Patient', :foreign_key => 'id'
+      t.belongs_to :doctor
+      t.belongs_to :patient
     end
   end
 end
