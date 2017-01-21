57a630114e62c8e7d09d492240267d1b649978e3
Scott Savage <snsavage@gmail.com>
2017-01-20 19:57:36 -0500

    Pass all lab specs

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,2 +1,8 @@
 class Appointment < ActiveRecord::Base
+  belongs_to :doctor
+  belongs_to :patient
+
+  def display
+    appointment_datetime.strftime("%B %e, %Y at %k:%M")
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170121002011) do
+ActiveRecord::Schema.define(version: 20170121002610) do
 
   create_table "appointments", force: :cascade do |t|
     t.datetime "appointment_datetime"
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 20170121002011) do
 
   create_table "patients", force: :cascade do |t|
     t.string   "name"
-    t.string   "age"
+    t.integer  "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
