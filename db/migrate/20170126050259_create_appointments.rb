c896db2aa5aa4053c2bda695d4ad0277856639dd
christinechoi <smi77y@gmail.com>
2017-01-26 00:08:50 -0500

    Done.

---
+++ db/migrate/20170126050259_create_appointments.rb
@@ -0,0 +1,8 @@
+class CreateAppointments < ActiveRecord::Migration
+  def change
+    create_table :appointments do |t|
+      t.datetime :appointment_datetime
+      t.timestamps null: false
+    end
+  end
+end
