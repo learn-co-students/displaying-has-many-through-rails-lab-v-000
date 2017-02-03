3f6559740cd8036fed10215915ecff52ab58384a
Adam Cooper <amcooper@gmail.com>
2017-02-03 12:37:44 -0500

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170203032636) do
+ActiveRecord::Schema.define(version: 20170203161414) do
 
   create_table "appointments", force: :cascade do |t|
     t.datetime "appointment_datetime"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20170203032636) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "age"
   end
 
 end
