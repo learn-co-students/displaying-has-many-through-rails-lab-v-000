a341a0283fb396df7033a3db619445445fa3503e
github username <github email address>
2017-02-26 20:12:52 -0500

    completed

---
+++ db/migrate/20170227004418_create_appointments.rb
@@ -0,0 +1,11 @@
+class CreateAppointments < ActiveRecord::Migration
+  def change
+    create_table :appointments do |t|
+      t.references :doctor, index: true, foreign_key: true
+      t.references :patient, index: true, foreign_key: true
+      t.datetime :appointment_datetime
+
+      t.timestamps null: false
+    end
+  end
+end
