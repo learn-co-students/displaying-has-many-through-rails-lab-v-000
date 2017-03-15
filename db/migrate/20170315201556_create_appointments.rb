0d30438a72f6eacf6995d7e908f875fe4b61393f
github username pianistps <github email address pianistps@yahoo.com>
2017-03-15 15:21:51 -0500

    add relationships to models and ids to belongs to

--- db/migrate/20170315201556_create_appointments.rb
+++ db/migrate/20170315201556_create_appointments.rb
@@ -2,6 +2,8 @@ class CreateAppointments < ActiveRecord::Migration
   def change
     create_table :appointments do |t|
       t.datetime :appointment_datetime
+      t.belongs_to :doctor
+      t.belongs_to :patient
 
       t.timestamps null: false
     end
