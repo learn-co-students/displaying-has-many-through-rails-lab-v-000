d30bc921929be9042912309175317bcc6777b5b3
Callie Brazil <cmabrazil@gmail.com>
2017-04-02 16:42:59 +0000

    Done.

---
+++ app/controllers/patients_controller.rb
@@ -0,0 +1,11 @@
+class PatientsController < ApplicationController
+
+  def index
+    @patients = Patient.all
+  end
+
+  def show
+    @patient = Patient.find(params[:id])
+    @appointments = @patient.appointments
+  end
+end
