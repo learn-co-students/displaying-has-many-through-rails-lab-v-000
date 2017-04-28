6c3094af100f773cac755e10d70bcf9fe45b09bd
Howard DeVennish <howarddevennish@gmail.com>
2017-04-28 12:34:55 +0000

    set up associations and run migrations

--- db/migrate/20170428122353_create_appointments.rb
+++ db/migrate/20170428122353_create_appointments.rb
@@ -2,6 +2,8 @@ class CreateAppointments < ActiveRecord::Migration
   def change
     create_table :appointments do |t|
       t.datetime :appointment_datetime
+      t.integer :doctor_id
+      t.integer :patient_id
 
       t.timestamps null: false
     end
