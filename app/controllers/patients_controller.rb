6b1d3124638bd24bd97334b2e70607c864f80394
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-07-05 18:28:18 -0600

    Done.

---
+++ app/controllers/patients_controller.rb
@@ -0,0 +1,14 @@
+class PatientsController < ApplicationController
+    def show
+        @patient = Patient.find(params[:id])
+    end
+
+    def index
+        @patients = Patient.all
+    end
+        
+private
+    def patient_params
+        params.require(:patient).permit(:name,:age)
+    end
+end
