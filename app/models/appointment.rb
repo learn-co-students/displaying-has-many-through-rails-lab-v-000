e5c146f6de90df315e9b0b1facb898d68a72dbbe
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-07-05 18:29:47 -0600

    Done.

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,12 @@
 class Appointment < ActiveRecord::Base
     belongs_to :doctor
     belongs_to :patient
+
+    def doctor_name
+        doctor.name
+    end
+
+    def patient_name
+        patient.name
+    end
 end
