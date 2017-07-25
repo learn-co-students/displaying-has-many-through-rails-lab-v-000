0cd04fd83ab22440850b6d110550f412056c48dd
James Fernandez <jffernan@comcast.net>
2017-07-25 04:17:47 +0000

    Done.

---
+++ db/migrate/20170725003043_create_patients.rb
@@ -0,0 +1,9 @@
+class CreatePatients < ActiveRecord::Migration
+  def change
+    create_table :patients do |t|
+      t.string :name
+      t.integer :age
+      t.timestamps null: false
+    end
+  end
+end
