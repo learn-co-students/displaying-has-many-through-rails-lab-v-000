44f4924dc694556ed76fd295e19fe815d8a29982
Sara Hopper <sara.hopper@gmail.com>
2017-07-26 22:10:15 +0000

    Done.

---
+++ app/controllers/patients_controller.rb
@@ -0,0 +1,26 @@
+class PatientsController < ApplicationController
+
+  def index
+    @patients = Patient.all
+  end
+
+  def show
+    @patient = Patient.find(params[:id])
+  end
+
+  def new
+    @patient = Patient.new
+  end
+
+  def create
+    patient = Patient.create(patient_params)
+    redirect_to patient_path
+  end
+
+  private
+
+  def patient_params
+    params.require(:patient).permit(:name, :age)
+  end
+
+end
