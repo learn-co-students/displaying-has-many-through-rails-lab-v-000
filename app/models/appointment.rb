abfaf94a55d2b957c80e01615bb7869f3552f864
hansenjl <jenn.leigh.hansen@gmail.com>
2017-08-15 11:03:09 -0700

    Done.

---
+++ app/models/appointment.rb
@@ -0,0 +1,12 @@
+class Appointment < ActiveRecord::Base
+  belongs_to :doctor
+  belongs_to :patient
+
+  def date_time
+    self.appointment_datetime.strftime('%B %d, %Y at %H:%M')
+  end
+end
+
+
+
+
