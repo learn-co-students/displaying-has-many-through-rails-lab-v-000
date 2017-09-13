3a9981ae1ac9fc235417ac29088e4bfdc6a5dffa
Amy B <aback1227@gmail.com>
2017-09-13 23:40:43 +0000

    Done.

---
+++ db/migrate/20170913224451_doctor.rb
@@ -0,0 +1,9 @@
+class Doctor < ActiveRecord::Migration
+  def change
+    create_table :doctors do |t|
+      t.string :name
+      t.string :department
+      t.timestamps null:false
+    end
+  end
+end
---
+++ db/migrate/20170913225330_patient.rb
@@ -0,0 +1,9 @@
+class Patient < ActiveRecord::Migration
+  def change
+    create_table :patients do |t|
+      t.string :name
+      t.integer :age
+      t.timestamps null:false
+    end
+  end
+end
