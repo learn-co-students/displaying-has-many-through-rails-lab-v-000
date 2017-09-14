125b1dabb312505235e501831579fb38e1955e01
James Hotchkiss <jimbohotchkiss@yahoo.com>
2017-09-14 16:27:47 +0000

    Done.

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,9 +2,6 @@ class Patient < ActiveRecord::Base
   has_many :appointments
   has_many :doctors, through: :appointments
 
-  def self.number_of_appointments
-    @patient = Patient.find(params[:id])
-    @patient_appointment_count = @patient.appointments.count
-  end
+
 
 end
