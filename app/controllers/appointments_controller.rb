50bbdf4e37269d31505485abeb34230de5157e49
James Hotchkiss <jimbohotchkiss@yahoo.com>
2017-09-14 15:05:10 +0000

    Done.

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -3,4 +3,11 @@ class AppointmentsController < ApplicationController
   def index
     @appointments = Appointment.all
   end
+
+  def show
+    @appointment = Appointment.find(params[:id])
+    @patient_name = @appointment.patient.name
+    @patient_age = @appointment.patient.age
+    @appointment_doctor_name = @appointment.doctor.name
+  end
 end
