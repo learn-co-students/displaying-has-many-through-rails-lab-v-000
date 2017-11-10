8d28f9f0ab1c17a0e6712d191d9237ea626c2b3f
Igor Eskin <github email address>
2017-11-10 13:41:41 -0500

    Created appointments/new form, changes in routes and controller

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -4,6 +4,16 @@ class AppointmentsController < ApplicationController
   #   @appointments = Appointment.all
   # end
 
+  def new
+    @appointment = Appointment.new
+  end
+
+  def create
+    binding.pry
+    @appointment = Appointment.create(appointment_params)
+    redirect_to appointment_path(@appointment)
+  end
+
   def show
     set_appointment
     @datetime = @appointment.appointment_datetime.strftime("%B %d, %Y at %k:%M")
@@ -15,6 +25,18 @@ class AppointmentsController < ApplicationController
 
   def set_appointment
     @appointment = Appointment.find(params[:id])
+  end
+
+  def appointment_params
+    params.require(:appointment).permit(
+      "appointment_datetime(1i)",
+ "appointment_datetime(2i)",
+ "appointment_datetime(3i)",
+ "appointment_datetime(4i)",
+ "appointment_datetime(5i)",
+ "doctor_id",
+ "patient_id"
+    )
   end
 
 
