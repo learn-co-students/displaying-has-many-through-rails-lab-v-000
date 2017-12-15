8a8bb70c3069d292da4a83f97872ea6e227ba70d
Adam Tropp <adt6261@gmail.com>
2017-12-15 05:46:02 +0000

    Done.

---
+++ app/models/appointment.rb
@@ -0,0 +1,20 @@
+class Appointment < ActiveRecord::Base
+  belongs_to :doctor
+  belongs_to :patient
+
+  def doctor_name=(name)
+    self.doctor = Doctor.find_or_create_by(name: name)
+  end
+
+  def doctor_name
+    self.doctor.name if self.doctor
+  end
+
+  def patient_name=(name)
+    self.patient = Patient.find_or_create_by(name: name)
+  end
+
+  def patient_name
+    self.patient.name if self.patient
+  end
+end
