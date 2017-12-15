2adfb26d1052f3cce04e8c57ddfa6d39493d048c
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-15 21:40:26 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
 
+  get 'appointments/show'
+
+  get 'doctors/index'
+
+  get 'doctors/show'
+
+  get 'patients/index'
+
+  get 'patients/show'
+
   resources :doctors
   resources :patients
   resources :appointments, only: [:show]
