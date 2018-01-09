17b0391e31c2ede5e7f4cbb18b7aaa5484eac272
Scott Wakeman <github email address>
2018-01-09 14:12:27 -0800

    Done.

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,18 +1,16 @@
 class PatientsController < ApplicationController
+  
   def show
       @patient = Patient.find(params[:id])
-      if @patient.appointments.last.try(:name)
-        @patient.appointments.build
-      end
-    end
+  end
 
-    def index
-      @patients = Patient.all
-    end
+  def index
+    @patients = Patient.all
+  end
 
-    private
+  private
 
-    def patient_params
-      params.require(:patient).permit(:name, :age)
-    end
+  def patient_params
+    params.require(:patient).permit(:name, :age)
+  end
 end
