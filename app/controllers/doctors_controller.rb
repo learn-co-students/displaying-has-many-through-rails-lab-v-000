27130d526637e2326700411dd4969291b1cf2a2c
Eric McKinney <mckinney99@gmail.com>
2018-01-19 21:18:32 +0000

    Done.

---
+++ app/controllers/doctors_controller.rb
@@ -0,0 +1,10 @@
+class DoctorsController < ApplicationController
+
+  def show
+    @doctor = Doctor.find(params[:id])
+  end
+
+  def index
+    @doctor = Doctor.all
+  end
+end
---
+++ app/controllers/patients_controller.rb
@@ -0,0 +1,10 @@
+class PatientsController < ApplicationController
+
+  def show
+    @patient = Patient.find(params[:id])
+  end
+
+  def index
+    @patient = Patient.all
+  end
+end
