4889060c0ba5a6f6da3db5c0a5fdce9bbba98c6c
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-02-05 23:18:51 +0000

    Done.

---
+++ app/controllers/appointments_controller.rb
@@ -0,0 +1,11 @@
+class AppointmentsController < ApplicationController
+
+  def new
+    @appointment = Appointment.new
+  end
+
+  def show
+    @appointment = Appointment.find(params[:id])
+  end
+  
+end
