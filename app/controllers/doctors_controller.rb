99f8054daac889414d48a35d21dd1a2b01bcdb04
daniellecroxton <danikrist@gmail.com>
2018-02-08 21:51:59 -0700

    Done.

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,8 +1,9 @@
 class DoctorsController < ApplicationController
   def index
+    @doctors = Doctor.all
   end
 
   def show
-    @doctor = Doctor.find(params[:id])
+    @doctor = Doctor.find_by_id(params[:id])
   end
 end
--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,8 @@
 class Appointment < ActiveRecord::Base
   belongs_to :doctor
   belongs_to :patient
+
+  def datetime_formatted
+    self.appointment_datetime.strftime("%B %d, %Y at %k:%M")
+  end
 end
