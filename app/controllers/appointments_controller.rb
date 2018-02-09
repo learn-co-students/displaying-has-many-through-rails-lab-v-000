4afc1bec50b7d6efc3094e85199574019d44b6d9
Chris Gullickson <cgullickson5@gmail.com>
2018-02-09 01:34:42 +0000

    Done.

---
+++ app/controllers/appointments_controller.rb
@@ -0,0 +1,7 @@
+class AppointmentsController < ApplicationController
+  def show
+    @appointment = Appointment.find(params[:id])
+    @doctor = @appointment.doctor
+    @patient = @appointment.patient
+  end
+end
