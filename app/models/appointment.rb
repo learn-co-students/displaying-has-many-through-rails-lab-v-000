649c004b9db44b7208248a2572514c82ede7ce33
Adam <github email address>
2018-02-21 19:13:52 -0700

    Done.

---
+++ app/models/appointment.rb
@@ -0,0 +1,16 @@
+class Appointment < ActiveRecord::Base
+  belongs_to :doctor
+  belongs_to :patient
+
+  def doctor_name
+    self.doctor.name
+  end
+
+  def patient_name
+    self.patient.name
+  end
+
+  def date_and_time
+    appointment_datetime.strftime('%B %d, %Y') + " at " + appointment_datetime.strftime('%H:%M')
+  end
+end
