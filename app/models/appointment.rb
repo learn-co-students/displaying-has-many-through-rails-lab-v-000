411e853cee9629806376b28d9a02bfbfe7811fca
Alexander Urizar <urizaraj@gmail.com>
2018-03-14 19:03:23 +0000

    Done.

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,6 +3,6 @@ class Appointment < ActiveRecord::Base
   belongs_to :doctor
 
   def date_string
-    self.appointment_datetime.strftime('%B %e, %Y at %l:%M')
+    self.appointment_datetime.strftime('%B %e, %Y at %k:%M')
   end
 end
