16d5982bc2d564b0499f042148c7447120b87ad2
funemployeddad <funemployeddad@gmail.com>
2018-03-19 13:42:12 -0700

    doctors page displays appointment's patient name. need to figure out datetime issue

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,2 +1,17 @@
 class DoctorsController < ApplicationController
+  before_action :set_doctor, only: [:show]
+
+  
+  def index
+    @docters = Doctors.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_doctor
+    @doctor = Doctor.find(params[:id])
+  end
 end
