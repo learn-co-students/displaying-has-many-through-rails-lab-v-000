94b4b328348426936d3481624b74127c56f84369
github username <github email address>
2018-03-19 12:17:32 -0700

    migrations built, db seeded. working through associations

--- db/migrate/20180319190459_create_doctors.rb
+++ db/migrate/20180319190459_create_doctors.rb
@@ -1,7 +1,8 @@
 class CreateDoctors < ActiveRecord::Migration
   def change
     create_table :doctors do |t|
-
+      t.string :name
+      t.string :department
       t.timestamps null: false
     end
   end
