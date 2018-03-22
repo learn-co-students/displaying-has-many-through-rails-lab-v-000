7c2873f2498226485804d7015fc50d16b6a999b6
remclay <remmy.lauren@gmail.com>
2018-03-22 16:16:42 +1000

    Done.

---
+++ app/controllers/patients_controller.rb
@@ -0,0 +1,12 @@
+require 'pry'
+
+class PatientsController < ApplicationController
+
+  def index
+    @patients = Patient.all
+  end
+
+  def show
+    @patient = Patient.find_by_id(params[:id])
+  end
+end
