4493cae36d43128c279df36df5d7ebc81f62f560
anthonygharvey <anthony@anthonygharvey.com>
2018-04-23 00:46:25 -0400

    Done.

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,2 +1,15 @@
 class PatientsController < ApplicationController
+	before_action :set_patient!, only: [:show, :edit, :update, :destroy]
+
+	def index
+		@patients = Patient.all
+	end
+	def show
+	end
+
+	private
+	def set_patient!
+		@patient = Patient.find(params[:id])
+	end
+
 end
