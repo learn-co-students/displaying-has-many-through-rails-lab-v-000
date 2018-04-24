0774e69461e3141d5726387b107594af92ba694e
Brian Bytnar <bytnarb2@gmail.com>
2018-04-24 16:10:29 -0400

    Done.

---
+++ app/models/appointment.rb
@@ -0,0 +1,9 @@
+class Appointment < ActiveRecord::Base
+  belongs_to :patient
+  belongs_to :doctor
+
+  def readable_time
+    self.appointment_datetime.strftime('%B %d, %Y at %H:%M')
+  end
+  
+end
