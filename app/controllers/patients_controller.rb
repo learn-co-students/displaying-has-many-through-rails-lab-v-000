744d4800eee6cea1eed1e7650f6ae1595f4f28f0
katrpilar <kbrinso@gmail.com>
2018-04-23 18:24:25 -0700

    Completed

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,8 +1,11 @@
 class PatientsController < ApplicationController
   def index
+  	@patients = Patient.all
   end
 
   def show
+  	@patient = Patient.find(params[:id])
+  	@appointments = @patient.appointments
   end
 
   def new
