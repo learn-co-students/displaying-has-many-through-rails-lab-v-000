549a8358421b8cd8f96dfd6b96ddaf3146d3bf6d
MyMac <mymac@MyMacs-MacBook-Pro.local>
2018-04-30 15:26:53 -0700

    Done.

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,11 +1,26 @@
 class DoctorsController < ApplicationController
+	before_action :set_doctor, only: [:show, :edit, :update, :destroy]
 
 	def index 
 		@doctors = Doctor.all 
 	end 
 
-	def show 
-		@doctor = Doctor.find(params[:id])
+	def new 
+		@doctor = Doctor.new 
 	end 
-	
+
+	def show 
+		
+	end 
+
+
+	private 
+
+		def doctor_params 
+			params.require(:doctor).permit(:name, :department)
+		end 
+
+		def set_doctor
+			@doctor = Doctor.find(params[:id])
+		end 
 end
--- spec/controllers/doctor__no_test_framework_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe DoctorNoTestFrameworkController, type: :controller do
-
-end
