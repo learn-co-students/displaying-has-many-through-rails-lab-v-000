5b0c585b2f937dcee943936e99077d68cdd0a86e
Sean Oughton <seanoughton@gmail.com>
2018-05-01 20:10:08 +0000

    Done.

---
+++ app/controllers/doctors_controller.rb
@@ -0,0 +1,35 @@
+class DoctorsController < ApplicationController
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def doctor_params
+    params.require().permit()
+  end
+end
---
+++ app/controllers/patients_controller.rb
@@ -0,0 +1,35 @@
+class PatientsController < ApplicationController
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def patient_params
+    params.require().permit()
+  end
+end
