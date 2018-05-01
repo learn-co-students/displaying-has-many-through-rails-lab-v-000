c120749133ac466751fb8b127b592d7c2271f863
Sean Oughton <seanoughton@gmail.com>
2018-05-01 20:00:15 +0000

    Done.

--- db/migrate/20180501195635_create_appointments.rb
+++ db/migrate/20180501195635_create_appointments.rb
@@ -1,7 +1,7 @@
 class CreateAppointments < ActiveRecord::Migration
   def change
     create_table :appointments do |t|
-      t. :appointment_datetime
+      t.datetime :appointment_datetime
       t.belongs_to :doctor
       t.belongs_to :patient
       t.timestamps null: false
