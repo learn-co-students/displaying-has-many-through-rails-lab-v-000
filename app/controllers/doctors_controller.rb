04ef6c9c43e40f1ec8ed74dc97bef163919a422e
jljardon <github email address>
2018-05-01 01:32:16 -0400

    Done.

---
+++ app/controllers/doctors_controller.rb
@@ -0,0 +1,10 @@
+class DoctorsController < ApplicationController
+  before_action :set_doctor, only: %i[show edit update destroy]
+  def show; end
+
+  private
+
+  def set_doctor
+    @doctor = Doctor.find(params[:id])
+  end
+end
