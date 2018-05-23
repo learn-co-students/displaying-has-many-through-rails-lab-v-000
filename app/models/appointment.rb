013135c48bb46042c9d941e9e9c4faecd82e7de2
ElleeB <laura.trager@gmail.com>
2018-05-23 08:50:23 -0400

    Troubleshooting Patient#show patient.name - not recognizing params[:id]

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -5,7 +5,7 @@ class Appointment < ActiveRecord::Base
   belongs_to :doctor
 
   def date
-    self.appointment_datetime.strftime("%B %d, %Y at %I:%M")
+    self.appointment_datetime.strftime("%B %d, %Y at %H:%M")
   end
 
 end
