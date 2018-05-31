090c510cd56654fd8392f163359eb14ae7f2d4fb
AlexFriedland <friedlandaj@gmail.com>
2018-05-31 15:44:07 +0200

    doctor done

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -2,5 +2,9 @@ class DoctorsController < ApplicationController
 
   def show
     @doctor = Doctor.find(params[:id])
+
+    @doctor.appointments.each do |appt|
+      @patient = Patient.find_by(id: appt.patient_id)
+    end
   end
 end
