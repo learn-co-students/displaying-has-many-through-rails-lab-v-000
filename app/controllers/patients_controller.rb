185ffee6f2139be933bb150e7811680b4970425d
AlexFriedland <friedlandaj@gmail.com>
2018-05-31 15:56:36 +0200

    progress

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,5 +1,13 @@
 class PatientsController < ApplicationController
+  def index
+    @all = Patient.all
+  end
+
   def show
     @patient = Patient.find(params[:id])
+
+    @patient.appointments.each do |appt|
+      @doctor = Doctor.find_by(id: appt.doctor_id)
+    end
   end
 end
