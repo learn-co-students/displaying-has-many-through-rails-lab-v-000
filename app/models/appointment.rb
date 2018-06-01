01ea004d8958e7692fea5881df6fc0a6a110695d
Kim Gonzales <Kimgo589@gmail.com>
2018-06-01 15:50:18 -0400

    Done.

---
+++ app/models/appointment.rb
@@ -0,0 +1,8 @@
+class Appointment < ActiveRecord::Base
+    belongs_to :doctor
+    belongs_to :patient
+
+    def date_time
+        self.appointment_datetime.strftime("%B %d, %Y at %H:%k")
+    end 
+end
