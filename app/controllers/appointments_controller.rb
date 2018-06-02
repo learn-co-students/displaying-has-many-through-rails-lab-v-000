08b1d12ae02273fb78b627bb2fc8004b23b2c840
Raiet Intraub <raietsaeed@gmail.com>
2018-06-02 00:18:09 +0000

    Done.

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,2 +1,8 @@
 class AppointmentsController < ApplicationController
+
+  def show
+    @appointment = Appointment.find(params[:id])
+    # puts "Appointment time = #{@appointment.appointment_datetime} || #{@appointment.appointment_time}"
+  end
+
 end
