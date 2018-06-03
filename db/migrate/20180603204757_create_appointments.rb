429af52e31a323bb847c05f809daabc73597d871
Aly Alidina <aly_alidina@hotmail.com>
2018-06-03 21:23:41 +0000

    Done.

--- db/migrate/20180603204757_create_appointments.rb
+++ db/migrate/20180603204757_create_appointments.rb
@@ -1,7 +1,6 @@
 class CreateAppointments < ActiveRecord::Migration
   def change
     create_table :appointments do |t|
-      t.string :name
       t.datetime :appointment_datetime
 
       t.timestamps null: false
