5a9f6bcce1e1de3138a1485b21ba364112e3ac14
Aly Alidina <aly_alidina@hotmail.com>
2018-06-05 23:23:34 -0400

    Done.

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,37 +1,8 @@
 class DoctorsController < ApplicationController
-  def index
-    @doctors = Doctor.all
-  end
-
-  def create
-    @doctor.create(doctor_params)
-    redirect_to doctor_path(@doctor)
-  end
-
-  def new
-    @doctor = Doctor.new
-  end
-
-  def edit
-
-  end
 
   def show
     @doctor = Doctor.find(params[:id])
   end
 
-  def update
-
-  end
-
-  private
-
-  def doctor_params
-    params.require(:doctors).permit(:name, :department, :accepts_nested_attributes_for [
-      :id,
-      :appointment_datetime,
-
-      ])
-  end
 
 end
