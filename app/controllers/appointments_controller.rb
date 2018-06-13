e1d1e801201898a4ac634f13a11369492bfa8289
ryanwevans <ryanwevans@icloud.com>
2018-06-12 17:38:15 -0700

    refactor to abstract out the Date/Time formatting into Appointment#date_time_formatted for DRY principle.

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -2,8 +2,4 @@ class AppointmentsController < ApplicationController
   def show
     @appointment = Appointment.find(params[:id])
   end
-
-  def date_time_formatted
-    self.appointment_datetime.strftime("%B %d, %G at %R")
-  end
 end
--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,10 @@
 class Appointment < ActiveRecord::Base
   belongs_to :doctor
   belongs_to :patient
+
+# Format the Date/Time to human readable format (i.e. "June 12, 2018 at 16:30")
+  def date_time_formatted
+    self.appointment_datetime.strftime("%B %d, %G at %R")
+  end
+
 end
