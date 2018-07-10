f8798f8db85ea5af096c3f413433663af4a259f4
SamanthaMGarcia <samantha.marie.garcia@gmail.com>
2018-07-09 21:34:57 -0400

    Done.

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -4,9 +4,9 @@ class AppointmentsController < ApplicationController
     @appointment = Appointment.find(params[:id])
   end
 
-  private
-
-  def appointment_params
-    params.require(:appointment).permit(:date_time, :patient, :doctor)
-  end
+  # private
+  #
+  # def appointment_params
+  #   params.require(:appointment).permit(:date_time, :patient, :doctor)
+  # end
 end
--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -5,5 +5,6 @@ class DoctorsController < ApplicationController
 
   def show
     @doctor = Doctor.find(params[:id])
+    @appointments = @doctor.appointments
   end
 end
