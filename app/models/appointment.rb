afe6b69b7170cd4987d70444aa2a5288ffdf7f5f
Karl Amoguis <ktamoguis@gmail.com>
2018-07-12 09:26:09 +0000

    Done.

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,9 @@
 class Appointment < ActiveRecord::Base
   belongs_to :doctor
   belongs_to :patient
+
+  def appointment_format
+    a = self.appointment_datetime
+    a.strftime("%B") + " " + a.strftime("%d") + ", " + a.strftime("%Y") + " at " + a.strftime("%H") + ":" + a.strftime("%M")
+  end
 end
