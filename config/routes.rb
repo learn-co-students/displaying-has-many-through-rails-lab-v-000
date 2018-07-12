9e22a49af2028fd22f0dd162d51b5d4ff1b2a340
Robin Doirin <robindoirin@gmail.com>
2018-07-12 16:58:20 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-
   resources :doctors
   resources :patients
-  resources :appointments, only: [:show]
+  resources :appointments, except: :index
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
