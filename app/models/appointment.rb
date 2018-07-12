0cddb6070f1361dbc39187f7657198b879f192ad
Zac Willmington <zac.willmington@gmail.com>
2018-07-12 17:49:48 +0000

    Done.

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,7 +3,7 @@ class Appointment < ActiveRecord::Base
     belongs_to :patient
 
     def parse_appointment_datetime
-        Date.parse(self.appointment_datetime).strftime("%B %d, %Y at 20:20")
+        self.appointment_datetime.strftime("%B %d, %Y at %H:%M")
     end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180712151005) do
+ActiveRecord::Schema.define(version: 20180712173755) do
 
   create_table "appointments", force: :cascade do |t|
-    t.string   "appointment_datetime"
+    t.datetime "appointment_datetime"
     t.integer  "doctor_id"
     t.integer  "patient_id"
     t.datetime "created_at",           null: false
