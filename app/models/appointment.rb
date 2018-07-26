56517b1ce69cf897bc48723f8ff2e60eda47cff6
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-07-26 20:52:22 +0000

    Done.

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,6 +1,7 @@
 class Appointment < ActiveRecord::Base
   belongs_to :doctor
   belongs_to :patient
+  after_find :readable_date_time
   
   def readable_date_time
     self.appointment_datetime.strftime("%B %A, %Y at %I:%M")
