ec81405d0b5a3a2a015dd84330f1ddb6835c1d0e
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-07-26 00:43:21 +0000

    Done.

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -6,6 +6,10 @@ class Appointment < ActiveRecord::Base
     self.patient.name
   end
 
+  def doctor_name
+    self.doctor.name
+  end
+
   def readable_datetime
     self.appointment_datetime.strftime("%B %e, %Y at %l:%M")
   end
