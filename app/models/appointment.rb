324cde16e86eac594b66d0ccf222bbc6e928fbd4
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-07-27 14:00:41 +0000

    Done.

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,20 +1,13 @@
 class Appointment < ActiveRecord::Base
   belongs_to :doctor
   belongs_to :patient
-  validate :is_readable_datetime 
   before_validation :make_readable_datetime
 
   private
   
- 
-  def is_readable_time
-    if appointment_datetime != appointment_datetime.strftime("%B %e, %Y at %l:%M")
-      errors.add(:appointment_datetime, "Time must be readable")
-    end
-  end
   
   def make_readable_datetime 
-    self.appointment_datetime = self.appointment_datetime.strftime("%B %e, %Y at %l:%M")
+    self.appointment_datetime = self.appointment_datetime.strftime("%B %e, %Y at %H:%M")
   end
   
 end
