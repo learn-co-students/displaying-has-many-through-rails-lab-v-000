5908267c22685d108bd2b9c00ca471e13448f7ab
taryn yaeger <tsyaeger@gmail.com>
2018-07-29 20:14:00 +0000

    Done.

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,2 +1,16 @@
 class AppointmentsController < ApplicationController
+  
+  def index 
+    @appointments = Appointment.all
+  end 
+  
+  def show 
+    @appointment = Appointment.find(params[:id])
+  end
+  
+  
+    
+  
+  
+  
 end
