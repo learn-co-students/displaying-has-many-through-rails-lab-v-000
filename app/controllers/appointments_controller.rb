5f14d983fdd2761595be874528d02e792eacdf14
Suchitra Naik <susanaik@gmail.com>
2018-08-06 17:06:54 +0000

    Done.

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -18,5 +18,6 @@ class AppointmentsController < ApplicationController
   end
 
   def show
+    @appointment = Appointment.find_by_id(params[:id])
   end
 end
--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -15,8 +15,10 @@ class PatientsController < ApplicationController
   end
 
   def index
+    @patients = Patient.all
   end
 
   def show
+    @patient = Patient.find_by_id(params[:id])
   end
 end
