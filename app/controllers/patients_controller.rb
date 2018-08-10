a232c79bc59cd55559030a9c611be9d298f7cdf2
learn <albazaz@gmail.com>
2018-08-09 19:50:33 -0700

    Done.

---
+++ app/controllers/patients_controller.rb
@@ -0,0 +1,11 @@
+class PatientsController < ApplicationController
+
+    def show
+        @patient = Patient.find_by_id(params[:id])
+    end 
+
+    def index
+        @patients = Patient.all
+    end 
+
+end
