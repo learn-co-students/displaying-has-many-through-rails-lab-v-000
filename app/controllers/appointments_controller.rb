39bacaa0f5119fc73bbee2c834fd3870511f61a1
Brennan Fulmer <brennan.flmr@gmail.com>
2018-08-22 21:09:58 +0000

    Done.

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,2 +1,13 @@
+
 class AppointmentsController < ApplicationController
+  before_action :find_patient, only: [:show]
+  
+  def show 
+  end
+  
+  private 
+  
+  def find_patient
+    @patient = Patient.find(params[:id])
+  end
 end
--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,2 +1,13 @@
+
 class DoctorsController < ApplicationController
+  before_action :find_doctor, only: [:show]
+
+  def show 
+  end
+  
+  private 
+  
+  def find_patient
+    @patient = Patient.find(params[:id])
+  end
 end
