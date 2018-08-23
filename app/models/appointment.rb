7a969665d7773d43d83330566b31f5299199f668
Brennan Fulmer <brennan.flmr@gmail.com>
2018-08-23 01:25:03 +0000

    Done.

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -4,7 +4,7 @@ class Appointment < ActiveRecord::Base
   belongs_to :patient
   
   def formatted_date
-    testy = self.appointment_datetime.strftime('%B %d, %Y at %k:%M')
+    self.appointment_datetime.strftime('%B %d, %Y at %k:%M')
   end
   
 end
