8141059783f5c3e833838247cd403451ac018378
epic0448 <samuelogundiran@Yahoo.com>
2018-08-23 08:41:29 -0400

    updated

--- db/migrate/20180823022148_create_appointments.rb
+++ db/migrate/20180823022148_create_appointments.rb
@@ -1,10 +1,12 @@
 class CreateAppointments < ActiveRecord::Migration
   def change
     create_table :appointments do |t|
+      # when you do the "t.belongs_to :doctor" in the joins table... IT creates the doctor_id
+      # in the schemea altomaticly
       t.belongs_to :doctor
       t.belongs_to :patient
       t.string     :department
-      
+
       t.datetime :appointment_datetime
     end
   end
