0a501da9bf74c4c53e293cb5b5183ba5bdd6e026
pcherian10 <pcherian10@gmail.com>
2018-08-29 14:22:15 -0400

    Done.

---
+++ db/migrate/20180829153833_add_column_doctor_id_to_appointments.rb
@@ -0,0 +1,5 @@
+class AddColumnDoctorIdToAppointments < ActiveRecord::Migration
+  def change
+    add_column :appointments, :doctor_id, :integer
+  end
+end
---
+++ db/migrate/20180829153857_add_column_patient_id_to_appointments.rb
@@ -0,0 +1,5 @@
+class AddColumnPatientIdToAppointments < ActiveRecord::Migration
+  def change
+    add_column :appointments, :patient_id, :integer
+  end
+end
