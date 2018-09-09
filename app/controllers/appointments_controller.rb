180f6045582c10537cfe6edd71b8c3fb27dcc305
Adam Kalt <adam.kalt.1@gmail.com>
2018-09-09 00:01:40 +0000

    Done.

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -2,7 +2,8 @@ class AppointmentsController < ApplicationController
 
   def show
       @appointment = Appointment.find(params[:id])
+      @doctor = @appointment.doctor
+      @patient = @appointment.patient
     end
-  end
 
 end
--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -3,6 +3,5 @@ class DoctorsController < ApplicationController
 def show
     @doctor = Doctor.find(params[:id])
   end
-end
 
 end
