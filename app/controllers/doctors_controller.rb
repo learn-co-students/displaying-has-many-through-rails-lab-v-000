fce306d3817fc556b37814acd35a7fabc29dcd06
Brett Weisberg <weisberg.brett@gmail.com>
2018-09-25 23:41:10 +0000

    Done.

---
+++ app/controllers/doctors_controller.rb
@@ -0,0 +1,15 @@
+class DoctorsController < ApplicationController
+  def new
+    @doctor = Doctor.new
+  end
+
+  def create
+  end
+
+  def show
+
+  end
+
+  def index
+  end
+end
---
+++ app/controllers/patients_controller.rb
@@ -0,0 +1,13 @@
+class PatientsController < ApplicationController
+  def new
+    @patient = Patient.new
+  end
+
+  def create
+  end
+
+  def show
+
+  end
+
+end
