777b9a8dc5c8939024e2797a8f95a72c744d7076
github username <github email address>
2018-10-05 16:17:40 -0400

    Done.

---
+++ app/controllers/appointments_controller.rb
@@ -0,0 +1,12 @@
+class AppointmentsController < ApplicationController
+
+    def index
+        @appointments = Appointment.all
+    end
+
+    def show 
+        @appointment = Appointment.find(params[:id])
+    end
+
+
+end
