cf3b3acbc69fbceba8142a9ea5688e61e2a36173
BrittanyTinnin <bntinnin7@gmail.com>
2018-10-08 23:38:24 -0400

    Done.

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,4 +1,5 @@
 class PatientsController < ApplicationController
+  before_action :set_patient, only: [:show]
   def new
 
   end
@@ -12,6 +13,12 @@ class PatientsController < ApplicationController
   end
 
   def index
+    @patients = Patient.all
+  end
 
+  private
+
+  def set_patient
+    @patient = Patient.find(params[:id])
   end
 end
