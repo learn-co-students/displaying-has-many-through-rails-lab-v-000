e030e9e395495d79da7681aae88357d40c4aa4e9
Brenden Thornton <thorntonbrenden@gmail.com>
2018-11-24 09:59:13 -0700

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181123201448) do
+ActiveRecord::Schema.define(version: 20181124165720) do
 
   create_table "appointments", force: :cascade do |t|
     t.datetime "appointment_datetime"
