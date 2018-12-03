0b33067ce42e220a01761f19920893572997b182
Richard Sanchez <richard.charles.s@gmail.com>
2018-12-03 23:53:45 +0000

    Done.

---
+++ app/models/appointment.rb
@@ -0,0 +1,16 @@
+class Appointment < ActiveRecord::Base
+  belongs_to :doctor
+  belongs_to :patient
+
+  def patient_name
+    self.patient.name
+  end
+
+  def doctor_name
+    self.doctor.name
+  end
+
+  def time
+        self.appointment_datetime.strftime("%B %d, %Y at %H:%M")
+    end
+end
