fcf94d6a5ad8587b48a71e68882086d6e0249f79
Terry-Thompson <terry.lyn.thompson@gmail.com>
2019-01-03 13:59:11 -0600

    Done.

---
+++ app/controllers/appointments_controller.rb
@@ -0,0 +1,9 @@
+class AppointmentsController < ApplicationController
+  
+  def show
+    @appointment = Appointment.find(params[:id])
+    @doctor = Doctor.find(@appointment.doctor_id)
+    @patient = Patient.find(@appointment.patient_id)
+  end
+
+end
