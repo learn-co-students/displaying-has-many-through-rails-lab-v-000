01bf732a1297d6d84e9414da8a6fd4edfd67bc9d
Marc Mills <mdmills1990@gmail.com>
2019-01-05 12:08:05 -0500

    Done.

---
+++ app/models/appointment.rb
@@ -0,0 +1,9 @@
+class Appointment < ActiveRecord::Base
+  belongs_to :doctor
+  belongs_to :patient
+
+  def formatted_appt_time
+    self.appointment_datetime.strftime("%B %d, %Y at %H:%M")
+  end
+
+end
