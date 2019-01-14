f7b5a3b03efa15b56acfbcd8d8a51ab796ec2fa2
kev112 <werewaswas@live.com>
2019-01-13 19:34:03 -0800

    Done.

---
+++ app/controllers/appointments_controller.rb
@@ -0,0 +1,9 @@
+class AppointmentsController < ApplicationController
+    def index
+        @appointments = Appointment.all
+    end
+
+    def show
+        @appointment = Appointment.find(params[:id])
+    end
+end
