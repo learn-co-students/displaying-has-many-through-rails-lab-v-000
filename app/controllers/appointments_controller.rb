899961d962c5c632871da2650bf611551c87a521
Nikki A <nikki.apostolakis@gmail.com>
2019-01-30 20:17:07 +0000

    Done.

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,2 +1,11 @@
 class AppointmentsController < ApplicationController
+
+  def index
+    raise ActionController::RoutingError.new('Not Found')
+  end
+
+  def show
+    @appointment = Appointment.find(params[:id])
+  end
+
 end
