5107c61b81faafdfd94ec3daec45cdecdad6ca6a
Erin Green <erin.y.green@gmail.com>
2019-02-19 20:46:21 +0000

    Done.

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,11 @@
 class Appointment < ActiveRecord::Base
   belongs_to :patient
-  belongs_to :doctor 
+  belongs_to :doctor
+
+  def readable_date
+    date_time = self.appointment_datetime.to_formatted_s(:long_ordinal)
+    time = date_time.slice(-5..-1)
+    date = date_time.slice(0..-7)
+    "#{date} at #{time}"
+  end
 end
