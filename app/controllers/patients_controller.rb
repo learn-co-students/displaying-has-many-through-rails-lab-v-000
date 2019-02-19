e3b8f71f8756ea5050304998b9f04b0345f6ce8c
Erin Green <erin.y.green@gmail.com>
2019-02-19 21:18:35 +0000

    Done.

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,10 +1,13 @@
 class PatientsController < ApplicationController
 
-  def show
-    @patient = Patient.find(params[:id])
-  end
 
   def index
     @patients = Patient.all
   end
+  
+  def show
+    @patient = Patient.find(params[:id])
+  end
+
+
 end
