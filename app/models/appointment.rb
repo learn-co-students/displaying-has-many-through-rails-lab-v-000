ae55d81116f8bae5ed3fe456849fb77c2793087b
Erin Green <erin.y.green@gmail.com>
2019-02-19 21:19:20 +0000

    Done.

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,7 +3,7 @@ class Appointment < ActiveRecord::Base
   belongs_to :doctor
 
   def readable_date
-    binding.pry
+
     date_time = self.appointment_datetime.to_formatted_s(:long_ordinal)
     time = date_time.slice(-5..-1)
     date = date_time.slice(0..-7)
