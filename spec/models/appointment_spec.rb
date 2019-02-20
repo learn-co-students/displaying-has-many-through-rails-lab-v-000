562c0d83a74d232c1cd5bcce86fa32a4c3cc978d
James Borocz <thesilentc@me.com>
2019-02-20 13:55:07 -0500

    Done.

--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -1,23 +1,5 @@
 require 'rails_helper'
 
-describe 'Appointment' do
-
-  before do
-    @mcdreamy = Doctor.create(name: "Derek Shepherd", department: "Neurosurgery")
-    @patient = Patient.create(name: "Russell Wilson", age: 27)
-    @appointment = Appointment.create({appointment_datetime: DateTime.new(2016, 12, 25), patient: @patient, doctor: @mcdreamy})
-  end
-
-  it 'has a date and time' do
-    expect(@appointment.appointment_datetime.strftime('%B %d, %Y')).to eq('December 25, 2016')
-  end
-
-  it 'belongs to a patient' do
-    expect(@appointment.doctor).to be(@mcdreamy)
-  end
-
-  it 'belongs to a doctor' do
-    expect(@appointment.patient).to be(@patient)
-  end
-
+RSpec.describe Appointment, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
 end
