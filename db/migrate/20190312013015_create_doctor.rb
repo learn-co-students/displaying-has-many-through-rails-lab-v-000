fd6043e8656d3387319d25b4a0b232d98104e3f6
catherinism <catcreates13@gmail.com>
2019-03-11 21:53:15 -0400

    Done.

---
+++ db/migrate/20190312013015_create_doctor.rb
@@ -0,0 +1,10 @@
+class CreateDoctor < ActiveRecord::Migration
+  def change
+    create_table :doctors do |t|
+      t.string :name
+      t.string :department
+
+      t.timestamps null: false
+    end
+  end
+end
