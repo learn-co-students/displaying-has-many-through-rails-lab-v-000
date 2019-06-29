a03a732bd2e6f1988afc4772b40b5fc19da8a556
christianavdm <christianavdm@gmail.com>
2019-06-28 17:40:42 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :doctors
   resources :patients
-  resources :appointments, only: [:show]
+  get 'appointment/:id', to: 'appointments#show'
 end
