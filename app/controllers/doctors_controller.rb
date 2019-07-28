2848adac71d2a576b0c0f1baf5d6a9906daa1a52
NaKia Whitby <whitby.nakia@gmail.com>
2019-07-28 19:13:42 +0000

    Done.

---
+++ app/controllers/doctors_controller.rb
@@ -0,0 +1,19 @@
+class DoctorsController < ApplicationController
+
+  before_action :set_doctor, only: [:show]
+
+  def index
+    @doctors = Doctor.all
+  end
+
+  def show
+
+  end 
+
+ private
+
+  def set_doctor
+    @doctor = Doctor.find(params[:id])
+  end
+
+end
