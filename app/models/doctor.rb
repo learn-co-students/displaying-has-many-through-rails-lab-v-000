fa5cacf4f9d6bec9b24dfca55b0ea284b4ee7699
Paul Fell <swellpf@gmail.com>
2019-08-07 16:20:05 -0400

    Done.

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,12 +2,4 @@ class Doctor < ApplicationRecord
     has_many :appointments
     has_many :patients, through: :appointments
 
-    def appointments=(appointments)
-        self.appointments = Appointment.find(doctor_id: doctor.id)
-    end
-
-    def appointments
-     #  binding.pry
-       self.appointments ? self.appointments.datetime : nil
-    end
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,12 +2,4 @@ class Patient < ApplicationRecord
     has_many :appointments
     has_many :doctors, through: :appointments
 
-    def appointments=(appointments)
-        self.appointments = Appointment.find(patient_id: patient.id)
-    end
-
-    def appointments
-        #  binding.pry
-          self.appointments ? self.appointments.datetime : nil
-       end
 end
