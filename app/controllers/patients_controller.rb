0cbe2ad233d506c083e98e1b5396359c4f6a6264
dhintz <dhintz89@gmail.com>
2019-08-11 21:25:07 -0400

    Done.

---
+++ app/controllers/patients_controller.rb
@@ -0,0 +1,39 @@
+class PatientsController < ApplicationController
+  def index
+    @patients = Patient.all
+  end
+
+  def show
+    @patient = Patient.find(params[:id])
+  end
+
+  def new
+    @patient = Patient.new
+  end
+
+  def create
+    @patient = Patient.create(patient_params)
+    redirect_to patient_path(@patient)
+  end
+
+  def edit
+    @patient = Patient.find(params[:id])
+  end
+
+  def update
+    @patient = Patient.find(params[:id])
+    @patient.update(patient_params)
+    redirect_to patient_path(@patient)
+  end
+
+  def destroy
+    @patient = Patient.find(params[:id])
+    @patient.destroy
+    redirect_to patients_path
+  end
+
+  private
+  def patient_params
+    params.require(:patient).permit(:name, :age)
+  end
+end
---
+++ db/migrate/20190811224327_create_patients.rb
@@ -0,0 +1,10 @@
+class CreatePatients < ActiveRecord::Migration[5.0]
+  def change
+    create_table :patients do |t|
+      t.string :name
+      t.integer :age
+
+      t.timestamps
+    end
+  end
+end
