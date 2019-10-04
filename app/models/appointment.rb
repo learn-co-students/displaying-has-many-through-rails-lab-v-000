67cad09a958a565c6dc6c0b79830dbaf88bd4e06
Amil <github email address>
2019-10-04 04:27:15 -0400

    Done.

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -13,4 +13,12 @@ class Appointment < ApplicationRecord
  def doctor_name
    self.doctor ? self.doctor.name : nil
  end
+
+  def patient_name=(name)
+   self.patient = Patient.find_or_create_by(name: name)
+ end
+
+ def patient_name
+   self.patient ? self.patient.name : nil
+ end
 end
