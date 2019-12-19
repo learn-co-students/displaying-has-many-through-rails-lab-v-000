8585468aa03225892c6a6ad1ece1ba66cb57aaaa
sdcro <sdcrouse91@gmail.com>
2019-12-18 17:56:38 -0800

    Done.

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -5,4 +5,17 @@ class Appointment < ApplicationRecord
   def patient_name
     self.patient.name
   end
+
+  def date_and_time
+    self.appointment_datetime.strftime("%B %d, %Y at %H:%M")
+  end
+
+  def doctor_name # This can probably be refactored/combined with #patient_name.
+    self.doctor.name
+  end
+
+  def name_of(person)
+    # self.send(person).name
+    person.name # This may be better, since it clearly identifies what the person is.
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  # Add this to the README (and delete the "routes" word): 
+    # Also, we've written the routes for you in the `routes.rb` file, so be sure to use the `--skip-routes` flag. This prevents the Rails generators from adding any unnecessary routes.
+    # You can use however many flags you need in the generators, so something like `rails g model Post title:string content:text --no-test-framework --skip-routes` is perfectly valid.
+    # (If you want to see what other flags and options are available, you can run commands such as `rails g --help` and `rails g model --help`.)
+    # Mention in the commit message that this can be edited for better readability if they wish. Or, they can ignore it if they think it adds irrelevant information.
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :doctors
   resources :patients
