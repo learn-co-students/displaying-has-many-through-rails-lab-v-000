ec13644c238c913ebe1ec26d2cac9f8254c4da8a
Argonity <maurice.argoetti@gmail.com>
2020-02-26 14:50:24 -0500

    Done.

---
+++ app/controllers/doctors_controller.rb
@@ -0,0 +1,36 @@
+class DoctorsController < ApplicationController
+
+  def index
+    @doctors = Doctor.all
+  end
+
+  def show
+    @doctor = Doctor.find(params[:id])
+  end
+
+  def edit
+    @doctor = Doctor.find(params[:id])
+  end
+
+  def new
+    @doctor = Doctor.new
+  end
+
+  def create
+    @doctor = Doctor.new(doctor_params)
+
+    if @doctor.save
+      redirect_to @doctor
+    else
+      render :new
+    end
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+
+end
---
+++ app/controllers/patients_controller.rb
@@ -0,0 +1,3 @@
+class PatientsController < ApplicationController
+
+end
