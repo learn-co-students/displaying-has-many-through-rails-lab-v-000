71d35d6484b1c79324818a557bed2d16fa2eff1b
Sheena Bonilla <sheena.myb@gmail.com>
2020-03-15 10:32:52 +0000

    Done.

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,3 +1,4 @@
 class Patient < ApplicationRecord
-  
+  has_many :appointments
+  has_many :doctors, through: :appointments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200315100357) do
+ActiveRecord::Schema.define(version: 20200315102900) do
 
   create_table "appointments", force: :cascade do |t|
     t.datetime "appointment_datetime"
-    t.integer  "user_id"
+    t.integer  "patient_id"
     t.integer  "doctor_id"
     t.index ["doctor_id"], name: "index_appointments_on_doctor_id"
-    t.index ["user_id"], name: "index_appointments_on_user_id"
+    t.index ["patient_id"], name: "index_appointments_on_patient_id"
   end
 
   create_table "doctors", force: :cascade do |t|
