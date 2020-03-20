44ef0cab7db7697030bfd9528a3e53273a05677c
Sheena Bonilla <sheena.myb@gmail.com>
2020-03-20 04:48:08 +0000

    Done.

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,2 +1,26 @@
 class PatientsController < ApplicationController
+
+  def index
+    @patients = Patient.all
+  end
+
+  def new
+    @patient = Patient.new
+
+  end
+
+  def create
+    Patient.create(patient_params)
+    redirect_to patients_path
+  end
+
+  def show
+  end
+
+  private
+
+  def patients_params(*args)
+    params.require(:patient).permit(args*)
+  end
+
 end
