dadc73ab170d0e220dcc2880e9df783b74eabcd9
Sheena Bonilla <sheena.myb@gmail.com>
2020-03-20 05:40:42 +0000

    Done.

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,7 +1,32 @@
 class AppointmentsController < ApplicationController
 
   def show
-    
+    @appointment = Appointment.find(params[:id])
+    @appointment.appointment_datetime
+  end
+
+  def new
+    @appointment = Appointment.new
+  end
+
+  def create
+    @appointment = Appointment.create(appointment_params)
+
+    if @appointment.save
+      redirect_to @appointment
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def appointment_params
+    params.require(:appointment).permit(
+      :appointment_datetime,
+      :doctor_name,
+      :patient_name
+    )
   end
 
 end
--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,24 @@
 class Appointment < ApplicationRecord
   belongs_to :doctor
   belongs_to :patient
+
+  def doctor_name=(name)
+    self.doctor = Doctor.find_or_create_by(name: name)
+  end
+
+  def doctor_name
+    self.doctor ? self.doctor.name : nil
+  end
+
+  def patient_name=(name)
+    self.patient = Patient.find_or_create_by(name: name)
+  end
+
+  def patient_name
+    self.patient ? self.patient.name : nil
+  end
+
+  def convert_time
+    self.appointment_datetime.strftime("%B %d, %Y at %H:%M")
+  end
 end
