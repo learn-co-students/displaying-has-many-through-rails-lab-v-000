4fea6a5d2c07493143d7dcf120cc89276fb79f18
yurigurlie207 <adrienne.yao@gmail.com>
2020-07-22 20:21:38 -0400

    Done.

---
+++ db/migrate/20200722180849_create_appointments.rb
@@ -0,0 +1,10 @@
+class CreateAppointments < ActiveRecord::Migration[5.0]
+  def change
+    create_table :appointments do |t|
+      t.datetime :appointment_datetime
+      t.belongs_to :doctor
+      t.belongs_to :patient
+      t.timestamps
+    end
+  end
+end
