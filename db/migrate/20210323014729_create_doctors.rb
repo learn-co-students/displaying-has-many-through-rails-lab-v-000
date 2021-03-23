945f361eeff1baba50f719ea6e501d41a146db9f
Laura Messa Travin <laura.travin@gmail.com>
2021-03-23 02:34:28 +0000

    Done.

---
+++ db/migrate/20210323014729_create_doctors.rb
@@ -0,0 +1,9 @@
+class CreateDoctors < ActiveRecord::Migration[5.0]
+  def change
+    create_table :doctors do |t|
+      t.string :name
+      t.string :department
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20210323014758_create_appointments.rb
@@ -0,0 +1,10 @@
+class CreateAppointments < ActiveRecord::Migration[5.0]
+  def change
+    create_table :appointments do |t|
+      t.datetime :appointment_datetime
+      t.integer :doctor_id
+      t.integer :patient_id
+      t.timestamps
+    end
+  end
+end
