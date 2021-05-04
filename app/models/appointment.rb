10ebd1d4630385a4de1814173a302e6a79b72f31
Jesse Wenger <wengerj@gmail.com>
2021-05-04 02:38:39 +0000

    Done.

---
+++ app/models/appointment.rb
@@ -0,0 +1,12 @@
+class Appointment < ApplicationRecord
+  belongs_to :doctor
+  belongs_to :patient
+
+  def doctor_name
+    self.doctor.name
+  end
+
+  def patient_name
+    self.patient.name
+  end
+end
